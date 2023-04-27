58ae1e1120ac544b117d23295ea70f70113f1264
xko0 <germain.rgb@hotmail.fr>
2023-04-27 03:59:16 +0200

    app created, first commit

---
+++ front/components/MainPage.vue
@@ -0,0 +1,84 @@
+<!-- <template>
+  <div>
+    <h1>My Page</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title.rendered }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  async created() {
+    await this.fetchPosts()
+  },
+
+  methods: {
+    async fetchPosts() {
+      try {
+        const response = await fetch('http://devtest1.infinityfreeapp.com/wp-json/wp/v2/posts')
+        const data = await response.json()
+        this.posts = data
+        console.log(this.posts)
+      } catch (error) {
+        console.error(error, 'test')
+      }
+    }
+  }
+}
+</script> -->
+
+<template>
+  <div>
+    <h1>My Page</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title.rendered }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+  data() {
+    return {
+      posts: []
+    }
+  },
+
+  async created() {
+    await this.fetchPosts()
+  },
+
+  methods: {
+
+    async fetchPosts() {
+      try {
+        const corsProxyUrl = 'https://cors-anywhere.herokuapp.com/'
+        const serverUrl = 'http://devtest1.infinityfreeapp.com/wp-json/wp/v2/posts'
+        const response = await fetch(corsProxyUrl + serverUrl, {
+          headers: {
+            'Origin': 'https://noip.com' 
+          }
+        })
+        // const data = await response.json()
+        this.posts = response
+        console.log(this.posts)
+      } catch (error) {
+        console.error(error, 'test')
+      }
+    }
+  }
+}
+</script>
+
+
---
+++ front/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <MainPage/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
